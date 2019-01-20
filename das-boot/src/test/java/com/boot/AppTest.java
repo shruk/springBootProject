c2c908f16090b9d279ed134f811e2fa1851c6159
phillip <windowsyyf@gmail.com>
2019-01-20 16:48:05 -0500

    add testing.

--- das-boot/src/test/java/com/boot/AppTest.java
+++ das-boot/src/test/java/com/boot/AppTest.java
@@ -1,20 +1,18 @@
 package com.boot;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import com.boot.controller.HomeController;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        HomeController hc=new HomeController();
+        String result=hc.home();
+        assertEquals(result, "Home controller return this by spring boot!!");
     }
 }
