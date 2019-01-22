4dc8375c120946f784e9a79251633967ea347ab5
phillip <windowsyyf@gmail.com>
2019-01-21 20:45:48 -0500

    update

--- das-boot/src/test/java/com/boot/ShipwreckControllerWebIntegrationTest.java
+++ das-boot/src/test/java/com/boot/ShipwreckControllerWebIntegrationTest.java
@@ -1,7 +1,6 @@
 package com.boot;
 
 import java.io.IOException;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -17,8 +16,9 @@ import org.springframework.web.client.RestTemplate;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration
+@SpringApplicationConfiguration(App.class)
 @WebIntegrationTest
 public class ShipwreckControllerWebIntegrationTest{
 
@@ -26,13 +26,13 @@ public class ShipwreckControllerWebIntegrationTest{
     public void testListAll() throws IOException{
         RestTemplate restTemplate=new TestRestTemplate();
         ResponseEntity<String> response=restTemplate.getForEntity("http://localhost:8080/api/v1/shipwrecks",String.class);
-        //assertThat(response.getStatusCode(),equalTo(HttpStatus.OK));
+        assertThat(response.getStatusCode(),equalTo(HttpStatus.OK));
 
         ObjectMapper objectMapper=new ObjectMapper();
         JsonNode responseJson=objectMapper.readTree(response.getBody());
 
-        //assertThat(responseJson.isMissingNode(),is(false));
-        //assertThat(responseJson.toString(),equalTo(" "));
+        assertThat(responseJson.isMissingNode(),is(false));
+        assertThat(responseJson.toString(),equalTo("[]"));
     }
 
 }
